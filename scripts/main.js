5d1004d839c95b48ad28cc5b70c2de56a503e980
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-07-20 17:16:19 +0200

    added vue.js starter and html

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,64 @@
 
+Vue.createApp ({
+
+    data() {
+
+        return {
+            images: [
+                {
+                    image: "img/01.webp",
+                    title: "Marvel\'s Spiderman Miles Morales",
+                    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+                }, {
+                    image: "img/02.webp",
+                    title: "Ratchet & Clank: Rift Apart",
+                    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+                }, {
+                    image: "img/03.webp",
+                    title: "Fortnite",
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, {
+                    image: "img/04.webp",
+                    title: "Stray",
+                    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+                }, {
+                    image: "img/05.webp",
+                    title: "Marvel's Avengers",
+                    text: "Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+                }
+            ]
+
+        }
+
+    },
+
+    methods: {
+
+
+
+    }
+
+}).mount("#app")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 //Creates variable for the carousel container.
 const carouselContainerElement = document.getElementById("carousel_container");
 
@@ -127,8 +187,8 @@ function goToPreviousImg() {
     //Decreases "currentImgIndex", used later to add ".active" to previous img.
     currentImgIndex--;
 
-    /*"if" construct sets "currentImgIndex" to ".length" of "imgElements" when "click" goes under 
-    imgs number, goes forward to last img. */
+    //"if" construct sets "currentImgIndex" to ".length" of "imgElements" when "click" goes under 
+    //imgs number, goes forward to last img.
     if (currentImgIndex < 0) {
 
         currentImgIndex = imgElements.length - 1;
@@ -139,3 +199,5 @@ function goToPreviousImg() {
     imgElements[currentImgIndex].classList.add("active");
 
 }
+
+*/
